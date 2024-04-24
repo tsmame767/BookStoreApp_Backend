f5c95dd841bb1530055dab20ffde382a1b2bd038
tsmame767 <tsmane789@gmail.com>
2024-04-24 12:41:54 +0530

    [Tushar] : Project Architecture Design

---
+++ RepositoryLayer/Database/DBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Database
+{
+    public class DBContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string ConnectStr;
+
+        public DBContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            this.ConnectStr = _configuration.GetConnectionString("SqlConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(this.ConnectStr);
+    }
+}
